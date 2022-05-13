ddf24a1e73520da8ff4dc450f816f348c3427096
psb629 <psb629@gmail.com>
2022-05-13 11:28:42 +0900

    updated final_project.ipynb

--- cp.parallel.zsh
+++ cp.parallel.zsh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
- #dir_to=/mnt/ext6/NAS05
- #fname=/mnt/sda2/TM
+dir_to=/mnt/ext6/NAS05
+fname=/mnt/sda2/TM
+
+parallel -j 10 cp -r -n {} $dir_to/ ::: $fname
+
+ #dir_from=/mnt/sda2/TM
+ #dir_to=/mnt/ext6/NAS05/TM
  #
- #parallel -j 10 cp -r -n {} $dir_to/ ::: $fname
-
-dir_from=/mnt/sda2/WinterCamp2021
-dir_to=/mnt/ext6/WinterCamp2021
-
-cd $dir_from
-find -print0 | parallel -0 -j 10 cp -r -n {} $dir_to
+ #cd $dir_from
+ #find -print0 | parallel -0 -j 10 cp -r -n {} $dir_to
